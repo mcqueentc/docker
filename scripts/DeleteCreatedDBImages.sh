f8913721121bc154b1dc6d28a2e3dc42368cbc49
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-09-14 15:46:29 +0200

    updated scripts

--- scripts/DeleteCreatedDBImages.sh
+++ scripts/DeleteCreatedDBImages.sh
@@ -4,5 +4,5 @@ docker rmi -f chronix
 docker rmi -f opentsdb
 docker rmi -f kairosdb
 docker rmi -f graphite
-docker rmi -f tutum/influxdb
+docker rmi -f influxdb
 #docker rmi -f tutum/curl
---
+++ scripts/DeleteUntaggedImages.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker rmi -f $(docker images -f "dangling=true" -q)
