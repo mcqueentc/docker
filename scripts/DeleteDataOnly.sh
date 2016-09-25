7fa1b3f8edfea129d2b1910948b1cdfcd75ec3e4
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-09-25 14:13:19 +0200

    another graphite try

--- scripts/DeleteDataOnly.sh
+++ scripts/DeleteDataOnly.sh
@@ -1,24 +1,38 @@
 #!/usr/bin/env bash
 
 sudo rm -rf /mnt/tsdb-benchmark-data/graphite/
-sudo rm -rf /mnt/tsdb-benchmark-data/graphite_nginx
-sudo rm -rf /mnt/tsdb-benchmark-data/graphite_log
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/nginx
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/log
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/logrotate
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/statsd
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/storage
+sudo rm -rf /mnt/tsdb-benchmark-data/graphite/conf
 sudo rm -rf /mnt/tsdb-benchmark-data/chronix/
 sudo rm -rf /mnt/tsdb-benchmark-data/kairosdb/
 sudo rm -rf /mnt/tsdb-benchmark-data/influxdb/
 sudo rm -rf /mnt/tsdb-benchmark-data/opentsdb/
 
-sudo mkdir /mnt/tsdb-benchmark-data/graphite
-sudo mkdir /mnt/tsdb-benchmark-data/graphite_nginx
-sudo mkdir /mnt/tsdb-benchmark-data/graphite_log
+
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/nginx
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/log
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/logrotate
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/statsd
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/storage
+sudo mkdir /mnt/tsdb-benchmark-data/graphite/conf
 sudo mkdir /mnt/tsdb-benchmark-data/chronix
 sudo mkdir /mnt/tsdb-benchmark-data/kairosdb
 sudo mkdir /mnt/tsdb-benchmark-data/influxdb
 sudo mkdir /mnt/tsdb-benchmark-data/opentsdb
 
-sudo chmod 777 /mnt/tsdb-benchmark-data/graphite
-sudo chmod 777 /mnt/tsdb-benchmark-data/graphite_nginx
-sudo chmod 777 /mnt/tsdb-benchmark-data/graphite_log
+
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/nginx
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/log
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/logrotate
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/statsd
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/storage
+sudo chmod 777 /mnt/tsdb-benchmark-data/graphite/conf
 sudo chmod 777 /mnt/tsdb-benchmark-data/chronix
 sudo chmod 777 /mnt/tsdb-benchmark-data/kairosdb
 sudo chmod 777 /mnt/tsdb-benchmark-data/influxdb
