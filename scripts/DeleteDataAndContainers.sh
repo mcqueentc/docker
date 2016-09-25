3abedc280cb9d46f7bd21c8a6b5a5bb7d5a444dc
Sebastian Thürauf <sebastian.thuerauf@me.com>
2016-09-25 14:47:02 +0200

    update scripts

--- scripts/DeleteDataAndContainers.sh
+++ scripts/DeleteDataAndContainers.sh
@@ -4,24 +4,18 @@ docker rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -qf dangling=true)
 
 sudo rm -rf /mnt/tsdb-benchmark-data/graphite/
-sudo rm -rf /mnt/tsdb-benchmark-data/graphite_nginx
-sudo rm -rf /mnt/tsdb-benchmark-data/graphite_log
 sudo rm -rf /mnt/tsdb-benchmark-data/chronix/
 sudo rm -rf /mnt/tsdb-benchmark-data/kairosdb/
 sudo rm -rf /mnt/tsdb-benchmark-data/influxdb/
 sudo rm -rf /mnt/tsdb-benchmark-data/opentsdb/
 
 sudo mkdir /mnt/tsdb-benchmark-data/graphite
-sudo mkdir /mnt/tsdb-benchmark-data/graphite_nginx
-sudo mkdir /mnt/tsdb-benchmark-data/graphite_log
 sudo mkdir /mnt/tsdb-benchmark-data/chronix
 sudo mkdir /mnt/tsdb-benchmark-data/kairosdb
 sudo mkdir /mnt/tsdb-benchmark-data/influxdb
 sudo mkdir /mnt/tsdb-benchmark-data/opentsdb
 
 sudo chmod 777 /mnt/tsdb-benchmark-data/graphite
-sudo chmod 777 /mnt/tsdb-benchmark-data/graphite_nginx
-sudo chmod 777 /mnt/tsdb-benchmark-data/graphite_log
 sudo chmod 777 /mnt/tsdb-benchmark-data/chronix
 sudo chmod 777 /mnt/tsdb-benchmark-data/kairosdb
 sudo chmod 777 /mnt/tsdb-benchmark-data/influxdb
